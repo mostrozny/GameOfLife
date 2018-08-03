8402f936746f727bfaaeea99de7b43fe9d720120
mostrozny <ostrozny.michal@gmail.com>
2018-08-03 10:45:26 +0200

    update index selector

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-var GameOfLife = function (boardWidth, boardHeight){
-  this.width = boardWidth;
-  this.height = boardHeight;
-  this.board = document.getElementById('board');
-  this.cells = [];
-  this.createBoard = function (){
-      //stworzenie tablicy o podanych rozmiarach, szer i wysokosc glownego
-      //diva i wpychanie nowych divow do glownej tablicy
-      this.board.style.width = this.width*10 + 'px';
-      this.board.style.height = this.height*10 + 'px';
-      var divCount = this.width*this.height;
-      for (var i=0; i<divCount; i++){
-          var createdDiv = document.createElement('div');
-          document.getElementById('board').appendChild(createdDiv);
-      }
-      //wpychanie do nowej tablicy wszystkich nowych elementow div
-      var newBoard = document.getElementById('board');
-      var self = this;
-      newBoard.querySelectorAll('div').forEach(function (element){
-          self.cells.push(element);
-      });
+var GameOfLife = function (boardWidth, boardHeight) {
+    this.width = boardWidth;
+    this.height = boardHeight;
+    this.board = document.getElementById('board');
+    this.cells = [];
+    this.createBoard = function () {
+        //stworzenie tablicy o podanych rozmiarach, szer i wysokosc glownego
+        //diva i wpychanie nowych divow do glownej tablicy
+        this.board.style.width = this.width * 10 + 'px';
+        this.board.style.height = this.height * 10 + 'px';
+        var divCount = this.width * this.height;
+        for (var i = 0; i < divCount; i++) {
+            var createdDiv = document.createElement('div');
+            document.getElementById('board').appendChild(createdDiv);
+        }
+        //wpychanie do nowej tablicy wszystkich nowych elementow div
+        var newBoard = document.getElementById('board');
+        var self = this;
+        newBoard.querySelectorAll('div').forEach(function (element) {
+            self.cells.push(element);
+        });
 
-      //ozywianie i usmiercanie komorek
-      this.cells.forEach(function (e){
-         e.addEventListener('mouseover', function (){
-             e.classList.toggle('live');
-         })
-      });
-  }
+        //ozywianie i usmiercanie komorek
+        this.cells.forEach(function (e) {
+            e.addEventListener('mousedown', function () {
+                e.classList.toggle('live');
+            })
+        });
+        }
+    this.index = function (x, y) {
+        return x + (y * this.width);
+    };
+
+}
 
 
-};
 
 var game = new GameOfLife(100, 100);
 game.createBoard();
